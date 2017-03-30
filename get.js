d65d7885f85aebef6fdf91e1021bc56bf745ca27
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-03-30 11:05:24 -0400

    Update get.js

--- get.js
+++ get.js
@@ -1,29 +1,48 @@
 $.getJSON( "https://api.github.com/users/thedustyard/repos", function( data ) {
+   
    var items = "";
+   
    $.each( data, function( key, val ) {
-      var name, link, fullname, description, language, lastupdate;
+      
+      var name, 
+          link, 
+          fullname, 
+          description, 
+          language, 
+          lastupdate;
+      
       $.each(val, function(key, val){
+         
          switch(key) {
+               
             case "name":
                name = val;
                break;
+               
             case "full_name":
                fullname = val;
                break;
+               
             case "html_url":
                link = val;
                break;
+               
             case "description":
                description = val;
                break;
+               
             case "language":
                language = val;
                break;
+               
             case "created_at":
                lastupdate = val;
                break;
+               
          }
+         
       });
+      
       items += (`
          <div class='feeditem half'>
             <a class='link' href='${link}'>
@@ -35,6 +54,9 @@ $.getJSON( "https://api.github.com/users/thedustyard/repos", function( data ) {
             <i><div class='feeddate'>${lastupdate.split('T')[0]}</div></i>
           </div>
        `);
+      
    });
+   
    document.getElementById("repo").innerHTML = items;
+   
 });
