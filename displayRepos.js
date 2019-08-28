6ecaf8c487691eac100649b064e79520497fa61a
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-08-27 22:30:22 -0400

    Rewrite of website

---
+++ displayRepos.js
@@ -0,0 +1,93 @@
+window.onload = async () => {
+   // Fetch the info about the org
+   let org_response = await fetch("https://api.github.com/users/thedustyard/repos");
+   
+   if (!org_response.ok) {
+      console.error(`Error fetching repositories: ${response.status}, ${response.statusText}`);
+      return;
+   }
+
+   // Fetch language information
+   let lang_response = await fetch("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json");
+
+   if (!lang_response.ok) {
+      console.error(`Error fetching languages: ${response.status}, ${response.statusText}`);
+      return;
+   }
+
+   /** 
+    * @type {{
+    *    name: string,
+    *    html_url: string,
+    *    description: string,
+    *    language: string,
+    *    created_at: string,
+    *    updated_at: string
+    * }[]}
+    */
+   let repos = await org_response.json();
+
+   /**
+    * @type {{
+    *    [x: string]: {
+    *       color: string,
+    *       url: string
+    *    }
+    * }}
+    */
+   let langs = await lang_response.json();
+
+   // Get the wrapper element to put the repos into
+   let wrapper_element = document.getElementById("repositories");
+
+   // loop through the repos
+   for (let repo of repos) {
+      // Format the text for the dates
+      let created_at_text = dayjs(repo.created_at).format("MMMM D, YYYY");
+      let updated_at_text = dayjs(repo.updated_at).format("MMMM D, YYYY");
+
+      // Create the outer element
+      let element = document.createElement("div");
+      element.className = "repo";
+      element.style.borderColor = langs[repo.language].color;
+
+      // Create the title element
+      let title = document.createElement("a");
+      title.className = "title";
+      title.href = repo.html_url;
+      title.innerText = repo.name;
+      element.appendChild(title);
+
+      // Create the description element
+      let description = document.createElement("div");
+      description.className = "description";
+      description.innerText = repo.description;
+      element.appendChild(description);
+
+      // Create the created element
+      let created = document.createElement("div");
+      created.className = "created";
+      created.innerText = `Created on ${created_at_text}`
+      element.appendChild(created);
+
+      // Create the updated element
+      let updated = document.createElement("div");
+      updated.className = "updated";
+      updated.innerText = `Last updated on ${updated_at_text}`
+      element.appendChild(updated);
+
+      // Create the language element
+      let language = document.createElement("div");
+      language.className = "language";
+      element.appendChild(language);
+
+      let language_text = document.createElement("div");
+      language_text.className = "label";
+      language_text.innerText = repo.language;
+      language_text.style.color = langs[repo.language].color;
+      language.appendChild(language_text);
+      
+      // Add the repo element to the weapper
+      wrapper_element.appendChild(element);
+   }
+}
